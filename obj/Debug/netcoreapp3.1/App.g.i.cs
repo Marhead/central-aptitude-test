3552c1b6956c728238683943a103f2a4a3ed86f9
Marhead <clgns0415@gmail.com>
2022-01-25 19:53:08 +0900

    Update gitignore

--- obj/Debug/netcoreapp3.1/App.g.i.cs
+++ obj/Debug/netcoreapp3.1/App.g.i.cs
@@ -10,7 +10,6 @@
 //------------------------------------------------------------------------------
 
 using CentralAptitudeTest;
-using DevExpress.Xpf.DXBinding;
 using System;
 using System.Diagnostics;
 using System.Windows;
@@ -20,7 +19,6 @@ using System.Windows.Controls.Primitives;
 using System.Windows.Controls.Ribbon;
 using System.Windows.Data;
 using System.Windows.Documents;
-using System.Windows.Forms.Integration;
 using System.Windows.Ink;
 using System.Windows.Input;
 using System.Windows.Markup;
@@ -47,7 +45,7 @@ namespace CentralAptitudeTest {
         /// InitializeComponent
         /// </summary>
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "6.0.1.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "4.8.1.0")]
         public void InitializeComponent() {
             
             #line 5 "..\..\..\App.xaml"
@@ -62,7 +60,7 @@ namespace CentralAptitudeTest {
         /// </summary>
         [System.STAThreadAttribute()]
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "6.0.1.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "4.8.1.0")]
         public static void Main() {
             CentralAptitudeTest.App app = new CentralAptitudeTest.App();
             app.InitializeComponent();
